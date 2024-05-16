5bd667e43412332fcaf3f4ce1b722b08d49c2775
Bradford-Bot~ <StarrlightGhost@gmail.com>
2024-05-16 13:18:31 -0500

    05/16/24

--- BD.py
+++ BD.py
@@ -1,1 +1,10 @@
 print("Binary Decoder")
+def numbers():
+    a = 1
+question = input("Would you like to decode or encrypt? ").lower()
+if question == "decode":
+    plain = int(input("Input text: ")).split()
+
+if question == "encrypt":
+    print()
+    print("\033[31m"  "Still working on that! Come back soon!!!" "\033[0m" )
